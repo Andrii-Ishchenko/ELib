2602bec00af73afe732d7a77f876943adcece4d2
Ihor Herasymiv <igorgerasymiv@gmail.com>
2015-08-06 15:35:56 +0300

    services, dto and mapper templates added

---
+++ ELib/ELib.BL/Services/Abstract/IBaseService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ELib.BL.Services.Abstract
+{
+    public interface IBaseService<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "");
+
+        TEntity GetById(object id);
+
+        void Insert(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        void DeleteById(object id);
+
+        void Update(TEntity entity);
+    }
+}
---
+++ ELib/ELib.BL/Services/Concrete/BaseService.cs
@@ -0,0 +1,71 @@
+﻿using ELib.BL.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using ELib.DAL.Infrastructure.Abstract;
+
+namespace ELib.BL.Services.Concrete
+{
+    public class BaseService<TEntity> : IBaseService<TEntity> where TEntity : class
+    {
+        private readonly IUnitOfWorkFactory _factory;
+
+        public BaseService(IUnitOfWorkFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var uow = _factory.Create())
+            {
+                uow.Repository<TEntity>().Delete(entity);
+                uow.Save();
+            }
+        }
+
+        public void DeleteById(object id)
+        {
+            using (var uow = _factory.Create())
+            {
+                uow.Repository<TEntity>().DeleteById(id);
+                uow.Save();
+            }
+        }
+
+        public IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
+        {
+            using (var uow = _factory.Create())
+            {
+                return uow.Repository<TEntity>().Get(filter, orderBy, includeProperties);
+            }
+        }
+
+        public TEntity GetById(object id)
+        {
+            using (var uow = _factory.Create())
+            {
+                return uow.Repository<TEntity>().GetById(id);
+            }
+        }
+
+        public void Insert(TEntity entity)
+        {
+            using (var uow = _factory.Create())
+            {
+                uow.Repository<TEntity>().Insert(entity);
+                uow.Save();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (var uow = _factory.Create())
+            {
+                uow.Repository<TEntity>().Update(entity);
+                uow.Save();
+            }
+        }
+    }
+}
