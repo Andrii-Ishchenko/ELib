891142954a6ac1418abf34912351dd60ef2f1985
Ihor Herasymiv <igorgerasymiv@gmail.com>
2015-08-07 12:46:01 +0300

    genre controller added

---
+++ ELib/ELib.Web/ApiControllers/GenreController.cs
@@ -0,0 +1,35 @@
+﻿using ELib.BL.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ELib.Web.ApiControllers
+{
+    public class GenreController : ApiController
+    {
+        private readonly IGenreService _genreService;
+
+        public GenreController(IGenreService genreService)
+        {
+            _genreService = genreService;
+        }
+
+
+        [HttpGet]
+        public HttpResponseMessage GetGenres()
+        {
+            try
+            {
+                var genres = _genreService.GetAll();
+                return Request.CreateResponse(HttpStatusCode.OK, genres);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+    }
+}
