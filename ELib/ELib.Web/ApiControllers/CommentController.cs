9dc097caa8ae3fa05721092f5628c0f7495a0246
Yaroslav Udovichenko <Yaroslav_Udovichenko@epam.com>
2015-08-11 14:00:35 +0300

    Draft version CommentController

---
+++ ELib/ELib.Web/ApiControllers/CommentController.cs
@@ -0,0 +1,34 @@
+﻿using ELib.BL.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ELib.Web.ApiControllers
+{
+    public class CommentController : ApiController
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetComments()
+        {
+            try
+            {
+                var comments = _commentService.GetAll();
+                return Request.CreateResponse(HttpStatusCode.OK, comments);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+    }
+}
