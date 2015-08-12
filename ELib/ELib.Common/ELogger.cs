a7850521e21d2765e93ba386e286f1fdc42ff450
jackxlifer <zolan7zolan>
2015-08-12 17:57:52 +0300

    Overloaded loggers log methods in order to provide exception information into logs.

--- ELib/ELib.Common/ELogger.cs
+++ ELib/ELib.Common/ELogger.cs
@@ -20,9 +20,19 @@ namespace ELib.Common
             logger.Log(LogLevel.Trace, msg);
         }
 
+        public void Trace(String msg, Exception ex)
+        {
+            logger.Log(LogLevel.Trace, msg, ex,null);
+        }
+
         public void Debug(String msg)
         {
             logger.Log(LogLevel.Debug, msg);
+        }
+
+        public void Debug(String msg, Exception ex)
+        {
+            logger.Log(LogLevel.Debug, msg, ex, null);
         }
 
         public void Info(String msg)
@@ -30,9 +40,19 @@ namespace ELib.Common
             logger.Log(LogLevel.Info, msg);
         }
 
+        public void Info(String msg, Exception ex)
+        {
+            logger.Log(LogLevel.Info, msg, ex, null);
+        }
+
         public void Warn(String msg)
         {
             logger.Log(LogLevel.Warn, msg);
+        }
+
+        public void Warn(String msg, Exception ex)
+        {
+            logger.Log(LogLevel.Warn, msg, ex, null);
         }
 
         public void Error(String msg)
@@ -40,9 +60,19 @@ namespace ELib.Common
             logger.Log(LogLevel.Error, msg);
         }
 
+        public void Error(String msg, Exception ex)
+        {
+            logger.Log(LogLevel.Error, msg, ex, null);
+        }
+
         public void Fatal(String msg)
         {
             logger.Log(LogLevel.Fatal, msg);
+        }
+
+        public void Fatal(String msg, Exception ex)
+        {
+            logger.Log(LogLevel.Fatal, msg, ex, null);
         }
     }
 }
