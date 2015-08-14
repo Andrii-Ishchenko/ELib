96616739d2cbce7bfb870d77dbfe070cb51b0bbd
Andrii-Ishchenko <Andrii_Ishchenko1@epam.com>
2015-08-14 13:49:42 +0300

    image resizer added

---
+++ ELib/ELib.Common/ImageResizer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ELib.Common
+{
+    public static class ImageResizer
+    {
+        public static Image ResizeImage(Image source, int targetHeight, int targetWidth)
+        {
+            int sourceHeight = source.Height;
+            int sourceWidth = source.Width;
+
+            if (sourceHeight < targetHeight && sourceWidth < targetWidth)
+                return source;
+
+            double ratioX = (double)sourceWidth / targetWidth;
+            double ratioY = (double)sourceHeight / targetHeight;
+
+            double ratio = Math.Max(ratioX, ratioY);
+
+            int newY = (int)(sourceHeight / ratio);
+            int newX = (int)(sourceWidth / ratio);
+
+            Bitmap newImage = new Bitmap(newX, newY);
+
+            using(var g = Graphics.FromImage(newImage))
+                g.DrawImage(source, 0, 0, newX, newY);
+
+            return newImage;
+
+        }
+
+        public static void Test()
+        {
+            using (var image = Image.FromFile(@"d:\aspnet.png"))
+            using (var newImage = ResizeImage(image, 300, 400))
+            {
+                newImage.Save(@"d:\test.png", ImageFormat.Png);
+            }
+        }
+
+    }
+}
