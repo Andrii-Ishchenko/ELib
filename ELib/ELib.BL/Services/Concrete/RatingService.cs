24ecd8d6d0fad60827287f7cdc341421945e7d90
Yaroslav Udovichenko <Yaroslav_Udovichenko@epam.com>
2015-08-18 13:58:55 +0300

    Rating Draft

---
+++ ELib/ELib.BL/Services/Concrete/RatingService.cs
@@ -0,0 +1,16 @@
+﻿using ELib.BL.DtoEntities;
+using ELib.BL.Services.Abstract;
+using ELib.DAL.Infrastructure.Abstract;
+using ELib.Domain.Entities;
+
+namespace ELib.BL.Services.Concrete
+{
+    public class RatingService : BaseService<RatingBook, RatingBookDto>, IRatingService
+    {
+        public RatingService(IUnitOfWorkFactory factory) 
+            :base(factory) 
+        {
+
+        }
+    }
+}
