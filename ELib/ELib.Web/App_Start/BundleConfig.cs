3fb2814d3a3949bd69bcf27cf95e2688815ccbca
Andrii-Ishchenko <Andrii_Ishchenko1@epam.com>
2015-08-21 12:49:23 +0300

    Little change to bundle Layout of main page has been added.

--- ELib/ELib.Web/App_Start/BundleConfig.cs
+++ ELib/ELib.Web/App_Start/BundleConfig.cs
@@ -21,7 +21,15 @@ namespace ELib.Web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap*"));
+
+
+            //.Include("~/Content/site.css")
+            bundles.Add(new StyleBundle("~/Content/css")           
+                .Include("~/Content/bootstrap*"));
+
+
 
         }
     }
