b2b561367e69f3683e3b12c03bae995a2a0bd312
Iuliia Lobodzinska <forest.navka@gmail.com>
2015-08-25 14:39:02 +0300

    #31 : begining of single book view creation

---
+++ ELib/ELib.Web/Views/Home/book/BookController.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    angular.module("elib")
+           .controller("BookController", BooksController);
+
+    BooksController.$inject = ["dataServiceFactory", '$routeParams'];
+
+    function BooksController(dataServiceFactory, $routeParams) {
+        var vm = this;
+        vm.instance = dataServiceFactory.getById('books', $routeParams.id).query();
+        console.log(vm.instance);
+    }
+})();
