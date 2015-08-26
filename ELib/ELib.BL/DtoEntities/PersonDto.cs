b7e5480a07f8395c0befb6c3ccf3add535a03668
Andrii-Ishchenko <Andrii_Ishchenko1@epam.com>
2015-08-26 14:08:15 +0300

    Added Profile Service

---
+++ ELib/ELib.BL/DtoEntities/PersonDto.cs
@@ -0,0 +1,44 @@
+﻿using ELib.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELib.BL.DtoEntities
+{
+    public class PersonDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [StringLength(30)]
+        public string Phone { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Login { get; set; }
+
+        //[Column(TypeName = "date")]
+        public DateTime RegistrationDate { get; set; }
+
+        //[Column(TypeName = "date")]
+        public DateTime? LastActivationDate { get; set; }
+
+        public string ImageHash { get; set; }
+
+        public string AplicationUserId { get; set; }
+
+        public virtual ApplicationUser PersonUser { get; set; }
+    }
+}
---
+++ ELib/ELib.BL/Services/Concrete/ProfileService.cs
@@ -0,0 +1,21 @@
+﻿using ELib.BL.DtoEntities;
+using ELib.BL.Services.Abstract;
+using ELib.DAL.Infrastructure.Abstract;
+using ELib.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELib.BL.Services.Concrete
+{
+    public class ProfileService : BaseService<Person,PersonDto>, IProfileService
+    {
+        public ProfileService(IUnitOfWorkFactory factory)
+            : base(factory)
+        { }
+
+
+    }
+}
