374a3d91ab32e086a6dc559210602ef640f16948
Ihor Herasymiv <igorgerasymiv@gmail.com>
2015-08-26 14:12:30 +0300

    book list directive is added

---
+++ ELib/ELib.Web/Views/Home/book/bookListDirective.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('elib')
+        .directive('bookListDirective', bookListDirective);
+
+    function bookListDirective($compile) {
+        return {
+            restrict: 'A',
+            scope: {
+                books: '='
+            },
+            templateUrl: '/views/home/book/book-list-item.html'
+        }
+    }
+
+})();
