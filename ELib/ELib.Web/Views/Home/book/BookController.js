30687f1732bde20925bf4a1291d910bbf85f2abe
Iuliia Lobodzinska <forest.navka@gmail.com>
2015-08-27 13:59:51 +0300

    #31 BookView : general markup was finished

--- ELib/ELib.Web/Views/Home/book/BookController.js
+++ ELib/ELib.Web/Views/Home/book/BookController.js
@@ -1,12 +1,24 @@
 ﻿(function () {
     angular.module("elib")
-           .controller("BookController", BooksController);
+           .controller("BookController", BookController);
 
-    BooksController.$inject = ["dataServiceFactory", '$routeParams'];
+    BookController.$inject = ["dataServiceFactory", '$routeParams'];
 
-    function BooksController(dataServiceFactory, $routeParams) {
+    function BookController(dataServiceFactory, $routeParams) {
         var vm = this;
         vm.instance = dataServiceFactory.getById('books', $routeParams.id).query();
-        console.log(vm.instance);
+        vm.getFullStarsArray = function () {
+            var fullStarsNumb = parseInt(vm.instance.Rating);
+            var arr = [];
+            if (fullStarsNumb > 0) arr.length = fullStarsNumb;
+            return arr;
+        };
+
+        vm.getEmptyStarsArray = function () {
+            var EmptyStarsNumb = 10 - parseInt(vm.instance.Rating);
+            var arr = [];
+            if (EmptyStarsNumb > 0) arr.length = EmptyStarsNumb;
+            return arr;
+        };
     }
 })();
