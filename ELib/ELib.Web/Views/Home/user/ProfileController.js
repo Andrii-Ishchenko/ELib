b3925e6689b608e8433b63d0472855ebf1ae8792
Andrii-Ishchenko <Andrii_Ishchenko1@epam.com>
2015-08-28 13:47:04 +0300

    Profile Menu links now works properly and lead to corresponding html pages

--- ELib/ELib.Web/Views/Home/user/ProfileController.js
+++ ELib/ELib.Web/Views/Home/user/ProfileController.js
@@ -2,14 +2,42 @@
     angular.module("elib")
            .controller("ProfileController", ProfileController);
 
-    ProfileController.$inject = ["ProfileFactory","$scope"];
+    ProfileController.$inject = ["ProfileFactory", "$scope"];
 
     function ProfileController(ProfileFactory,$scope) {
         var vm = this;
 
+        $scope.showGeneralInfo = function () {
+            $scope.template.main = "/views/home/user/profile-general.html";
+        }
+
+        $scope.showRatings = function () {
+            $scope.template.main = "/views/home/user/profile-ratings.html";
+        }
+
+        $scope.showComments = function () {
+            $scope.template.main = "/views/home/user/profile-comments.html";
+        }
+
+        $scope.showFavs = function () {
+            $scope.template.main = "/views/home/user/profile-favs.html";
+        }
+
+        $scope.showBooksToRead = function () {
+            $scope.template.main = "/views/home/user/profile-books-to-read.html";
+        }
+
+        $scope.showAlreadyReadBooks = function () {
+            $scope.template.main = "/views/home/user/profile-already-read-books.html";
+        }
+
+        $scope.showSocialNetworks = function () {
+            $scope.template.main = "/views/home/user/profile-social-networks.html";
+        }
+
         $scope.template = {
             menu: "/views/home/user/profile-menu.html",
-            main: "/views/home/user/profile.html"
+            main: "/views/home/user/profile-general.html"
         }
 
         vm.profile = ProfileFactory.getCurrentUser().query();
