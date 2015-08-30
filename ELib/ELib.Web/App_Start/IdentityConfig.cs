fca8653aeda522dd1e09d981445119dc30d58427
GerasimIG <igorgerasymiv@gmail.com>
2015-08-30 18:49:04 +0300

    add authentication infrastructure

--- ELib/ELib.Web/App_Start/IdentityConfig.cs
+++ ELib/ELib.Web/App_Start/IdentityConfig.cs
@@ -13,28 +13,10 @@ using Microsoft.Owin.Security;
 using ELib.Domain.Entities;
 using ELib.DAL.Infrastructure.Concrete;
 
-namespace ELib.Web.App_Start
+namespace ELib.Web
 {
-
-    public class EmailService : IIdentityMessageService
-    {
-        public Task SendAsync(IdentityMessage message)
-        {
-            // Plug in your email service here to send an email.
-            return Task.FromResult(0);
-        }
-    }
-
-    public class SmsService : IIdentityMessageService
-    {
-        public Task SendAsync(IdentityMessage message)
-        {
-            // Plug in your SMS service here to send a text message.
-            return Task.FromResult(0);
-        }
-    }
-
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
@@ -51,7 +33,6 @@ namespace ELib.Web.App_Start
                 AllowOnlyAlphanumericUserNames = false,
                 RequireUniqueEmail = true
             };
-
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
@@ -61,51 +42,12 @@ namespace ELib.Web.App_Start
                 RequireLowercase = true,
                 RequireUppercase = true,
             };
-
-            // Configure user lockout defaults
-            manager.UserLockoutEnabledByDefault = true;
-            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
-            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
-
-            // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
-            // You can write your own provider and plug it in here.
-            manager.RegisterTwoFactorProvider("Phone Code", new PhoneNumberTokenProvider<ApplicationUser>
-            {
-                MessageFormat = "Your security code is {0}"
-            });
-            manager.RegisterTwoFactorProvider("Email Code", new EmailTokenProvider<ApplicationUser>
-            {
-                Subject = "Security Code",
-                BodyFormat = "Your security code is {0}"
-            });
-            manager.EmailService = new EmailService();
-            manager.SmsService = new SmsService();
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
             {
-                manager.UserTokenProvider =
-                    new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+                manager.UserTokenProvider = new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
             }
             return manager;
-        }
-    }
-
-    // Configure the application sign-in manager which is used in this application.
-    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
-    {
-        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
-            : base(userManager, authenticationManager)
-        {
-        }
-
-        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
-        {
-            return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
-        }
-
-        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
-        {
-            return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
         }
     }
 }
--- ELib/ELib.Web/Startup.cs
+++ ELib/ELib.Web/Startup.cs
@@ -1,13 +1,6 @@
 ﻿using Microsoft.Owin;
 using Owin;
-using ELib.DAL.Infrastructure.Concrete;
 using System;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security.Cookies;
-using ELib.Web.App_Start;
-using ELib.Domain.Entities;
-using Microsoft.Owin.Security.Google;
 
 [assembly: OwinStartup(typeof(ELib.Web.Startup))]
 
@@ -15,61 +8,70 @@ namespace ELib.Web
 {
     public class Startup
     {
-
         public void Configuration(IAppBuilder app)
         {
-
-            // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ELibDbContext.Create);
-            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
-
-            // Enable the application to use a cookie to store information for the signed in user
-            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
-            // Configure the sign in cookie
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login"),
-                Provider = new CookieAuthenticationProvider
-                {
-
-
-                    // Enables the application to validate the security stamp when the user logs in.
-                    // This is a security feature which is used when you change a password or add an external login to your account.  
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                validateInterval: TimeSpan.FromMinutes(30),
-                regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
-                }
-            });
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
-            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
-            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
-
-            // Enables the application to remember the second login verification factor such as phone or email.
-            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
-            // This is similar to the RememberMe option when you log in.
-            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
-
-            // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
-
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
-
-            app.UseFacebookAuthentication(
-              appId: "150029308665046",
-              appSecret: "b5b60220fe4c0820958f551a582cbf8b");
-
-            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            {
-                ClientId = "796445415684-pj1uigsehmv7u6bcbcavllim3etjtct5.apps.googleusercontent.com",
-                ClientSecret = "1qeurRA9MFH69vAAFC9s2SaA"
-            });
+            ConfigureAuth(app);
         }
+
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+        /*
+public void Configuration(IAppBuilder app)
+{
+
+   // Configure the db context, user manager and signin manager to use a single instance per request
+   app.CreatePerOwinContext(ELibDbContext.Create);
+   app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+   app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+   // Enable the application to use a cookie to store information for the signed in user
+   // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+   // Configure the sign in cookie
+   app.UseCookieAuthentication(new CookieAuthenticationOptions
+   {
+       AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+       LoginPath = new PathString("/Account/Login"),
+       Provider = new CookieAuthenticationProvider
+       {
+
+
+           // Enables the application to validate the security stamp when the user logs in.
+           // This is a security feature which is used when you change a password or add an external login to your account.  
+           OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+       validateInterval: TimeSpan.FromMinutes(30),
+       regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+       }
+   });
+   app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+   // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
+   app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+
+   // Enables the application to remember the second login verification factor such as phone or email.
+   // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
+   // This is similar to the RememberMe option when you log in.
+   app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+
+   // Uncomment the following lines to enable logging in with third party login providers
+   //app.UseMicrosoftAccountAuthentication(
+   //    clientId: "",
+   //    clientSecret: "");
+
+   //app.UseTwitterAuthentication(
+   //   consumerKey: "",
+   //   consumerSecret: "");
+
+   app.UseFacebookAuthentication(
+     appId: "150029308665046",
+     appSecret: "b5b60220fe4c0820958f551a582cbf8b");
+
+   app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+   {
+       ClientId = "796445415684-pj1uigsehmv7u6bcbcavllim3etjtct5.apps.googleusercontent.com",
+       ClientSecret = "1qeurRA9MFH69vAAFC9s2SaA"
+   });
+} */
     }
 }
