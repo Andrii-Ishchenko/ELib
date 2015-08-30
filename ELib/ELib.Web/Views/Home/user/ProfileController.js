bfd5d5b52d40cff55678ceb3ee121d29ed644e77
Andrii-Ishchenko <Andrii_Ishchenko1@epam.com>
2015-08-30 18:35:12 +0300

    Refactored profile-menu: a lot of function replaced with one general.

--- ELib/ELib.Web/Views/Home/user/ProfileController.js
+++ ELib/ELib.Web/Views/Home/user/ProfileController.js
@@ -6,39 +6,56 @@
 
     function ProfileController(ProfileFactory,$scope) {
         var vm = this;
-
-        $scope.showGeneralInfo = function () {
-            $scope.template.main = "/views/home/user/profile-general.html";
-        }
-
-        $scope.showRatings = function () {
-            $scope.template.main = "/views/home/user/profile-ratings.html";
-        }
-
-        $scope.showComments = function () {
-            $scope.template.main = "/views/home/user/profile-comments.html";
-        }
-
-        $scope.showFavs = function () {
-            $scope.template.main = "/views/home/user/profile-favs.html";
-        }
-
-        $scope.showBooksToRead = function () {
-            $scope.template.main = "/views/home/user/profile-books-to-read.html";
-        }
-
-        $scope.showAlreadyReadBooks = function () {
-            $scope.template.main = "/views/home/user/profile-already-read-books.html";
-        }
-
-        $scope.showSocialNetworks = function () {
-            $scope.template.main = "/views/home/user/profile-social-networks.html";
-        }
-
+       
         $scope.template = {
             menu: "/views/home/user/profile-menu.html",
             main: "/views/home/user/profile-general.html"
         }
+
+        $scope.links = {
+            "GeneralInfo": "/views/home/user/profile-general.html",
+            "Ratings": "/views/home/user/profile-ratings.html",
+            "Comments": '/views/home/user/profile-comments.html',
+            "Favourites": "/views/home/user/profile-favs.html",
+            "BooksToRead": "/views/home/user/profile-books-to-read.html",
+            "AlreadyReadBooks": "/views/home/user/profile-already-read-books.html",
+            "SocialNetworks": "/views/home/user/profile-social-networks.html"
+        }
+
+        $scope.showSection = function (name) {
+            $scope.template.main = $scope.links[name];
+
+        }
+
+        //$scope.showGeneralInfo = function () {
+        //    $scope.template.main = "/views/home/user/profile-general.html";
+        //}
+
+        //$scope.showRatings = function () {
+        //    $scope.template.main = "/views/home/user/profile-ratings.html";
+        //}
+
+        //$scope.showComments = function () {
+        //    $scope.template.main = "/views/home/user/profile-comments.html";
+        //}
+
+        //$scope.showFavs = function () {
+        //    $scope.template.main = "/views/home/user/profile-favs.html";
+        //}
+
+        //$scope.showBooksToRead = function () {
+        //    $scope.template.main = "/views/home/user/profile-books-to-read.html";
+        //}
+
+        //$scope.showAlreadyReadBooks = function () {
+        //    $scope.template.main = "/views/home/user/profile-already-read-books.html";
+        //}
+
+        //$scope.showSocialNetworks = function () {
+        //    $scope.template.main = "/views/home/user/profile-social-networks.html";
+        //}
+
+
 
         vm.profile = ProfileFactory.getCurrentUser().query();
         //console.log("profile: "+vm.profile);
