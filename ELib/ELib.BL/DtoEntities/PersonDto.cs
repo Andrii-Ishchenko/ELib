0c0dcfc123c187fc6b715dacd283e84c37a813c6
Andrii-Ishchenko <Andrii_Ishchenko1@epam.com>
2015-09-02 10:52:38 +0300

    Fixed person dto

--- ELib/ELib.BL/DtoEntities/PersonDto.cs
+++ ELib/ELib.BL/DtoEntities/PersonDto.cs
@@ -12,16 +12,11 @@ namespace ELib.BL.DtoEntities
     {
         public int Id { get; set; }
 
-        [Required]
         [StringLength(50)]
         public string FirstName { get; set; }
 
         [StringLength(50)]
         public string LastName { get; set; }
-
-        [Required]
-        [StringLength(50)]
-        public string Login { get; set; }
 
         public string ImageHash { get; set; }
 
