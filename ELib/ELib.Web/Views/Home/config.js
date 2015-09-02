b3f4357cd41d7c8c98d6437e1036e6c57f468b69
Ihor Herasymiv <igorgerasymiv@gmail.com>
2015-09-02 14:18:20 +0300

    create infrastructure for frontend authentication

--- ELib/ELib.Web/Views/Home/config.js
+++ ELib/ELib.Web/Views/Home/config.js
@@ -3,37 +3,46 @@
            .config(config);
 
     function config($routeProvider) {
-    $routeProvider
-        .when('/books', {
-            templateUrl: '/views/shared/two-column-layout.html',
-          controller : 'BooksController',
-          controllerAs : 'books'
-        })
-        .when('/book/:id', {
-            templateUrl: '/views/home/book/book.html',
-            controller: 'BookController',
-            controllerAs: 'book'
-        })
-        .when('/profile/',{
-            templateUrl: '/views/shared/two-column-layout.html',
-            controller: 'ProfileController',
-            controllerAs: 'profileCtrl'
-        })
-        .when('/authors', {
-            templateUrl: '/views/shared/two-column-layout.html',
-            controller: 'AuthorsController',
-            controllerAs:'authors'
-        })
-        .when('/author/:id', {
-            templateUrl: '/views/home/author/author.html',
-            controller: 'AuthorController',
-            controllerAs: 'author'
-        }
-        )
-      //.........
-      .otherwise({
-          redirectTo: 'books'
-      });
+        $routeProvider
+            .when('/books', {
+                templateUrl: '/views/shared/two-column-layout.html',
+                controller : 'BooksController',
+                controllerAs : 'books'
+            })
+            .when('/book/:id', {
+                templateUrl: '/views/home/book/book.html',
+                controller: 'BookController',
+                controllerAs: 'book'
+            })
+            .when('/profile/',{
+                templateUrl: '/views/shared/two-column-layout.html',
+                controller: 'ProfileController',
+                controllerAs: 'profileCtrl'
+            })
+            .when('/authors', {
+                templateUrl: '/views/shared/two-column-layout.html',
+                controller: 'AuthorsController',
+                controllerAs:'authors'
+            })
+            .when('/author/:id', {
+                templateUrl: '/views/home/author/author.html',
+                controller: 'AuthorController',
+                controllerAs: 'author'
+            })
+            .when('/login', {
+                templateUrl: '/views/home/login.html',
+                controller: 'LoginController',
+                controllerAs: 'loginCtrl'
+            })
+            .when('/registration', {
+                templateUrl: '/views/home/registration.html',
+                controller: 'RegistrationController',
+                controllerAs: 'registrationCtrl'
+            })
+        //.........
+            .otherwise({
+                redirectTo: 'books'
+            });
         }
    })();
 
