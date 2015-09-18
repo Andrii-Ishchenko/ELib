40dc8ea2630ddb817eb131f4b48a799415cb9367
Yaroslav Udovichenko <Yaroslav_Udovichenko@epam.com>
2015-09-18 12:04:13 +0300

    temp commit 4

--- ELib/ELib.Web/Views/Home/Comment/CommentsController.js
+++ ELib/ELib.Web/Views/Home/Comment/CommentsController.js
@@ -1,4 +1,13 @@
-﻿angular.module("elib")
-           .controller("CommentsController", function($scope){});
+﻿(function () {
+    angular.module("elib")
+           .controller("CommentsController", CommentsController);
 
- 
+    CommentsController.$inject = ["commentsRepository", '$routeParams'];
+
+    function CommentsController(commentsRepository, $routeParams) {
+        var vm = this;
+        vm.instance = commentsRepository.getCommentById().get({ id: $routeParams.id });
+        };
+
+    }
+})();
