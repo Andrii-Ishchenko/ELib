5337eb81df90900c5f6657ac3f6f35f35e5e4ccb
Iuliia Lobodzinska <forest.navka@gmail.com>
2015-09-24 11:49:39 +0300

    107 : paginations fixed added angular-bootstrap pagination directives to books. authors and publishers lists fixed paginations parameters

--- ELib/ELib.Web/Views/Home/book/BooksController.js
+++ ELib/ELib.Web/Views/Home/book/BooksController.js
@@ -2,12 +2,13 @@
     angular.module("elib")
            .controller("BooksController", BooksController);
 
-    BooksController.$inject = ["dataServiceFactory", '$routeParams'];
+    BooksController.$inject = ["dataServiceFactory", '$routeParams', "$location"];
 
-    function BooksController(dataServiceFactory, $routeParams) {
+    function BooksController(dataServiceFactory, $routeParams, $location) {
         var vm = this;
         vm.pageCount = ($routeParams.pageCount) ? $routeParams.pageCount : 5;
         vm.currPage = ($routeParams.pageNumb) ? $routeParams.pageNumb : 1;
+        //$location.search({ "pageCount": vm.pageCount, "pageNumb": vm.currPage });
         var parameters = {
             pageCount : vm.pageCount,
             pageNumb  : vm.currPage,
@@ -17,7 +18,11 @@
             genre     : $routeParams.genre,
             subgenre  : $routeParams.subgenre,
             year      : $routeParams.year
-    }
+        }
+
+        vm.pageChanged = pageChanged;
+            //$location.search("pageNumb", vm.currPage);
+
 
         var obj = dataServiceFactory.getService('books').get(parameters);
         obj.$promise.then(function (data) {
@@ -26,6 +31,12 @@
             vm.totalPages = Math.ceil(vm.totalCount / vm.pageCount);
             vm.pages = new Array(vm.totalPages);
         })
+        
+        function pageChanged() {
+            $location.search({ "pageCount": vm.pageCount, "pageNumb": vm.currPage });
+        }
+
+
       
 
         //activate();
