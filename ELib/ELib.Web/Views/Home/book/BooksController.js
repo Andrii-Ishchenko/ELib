6d83d5a621f81b3fb8ed63dd1934e92c3f7670aa
Andrii-Ishchenko <Andrii_Ishchenko1@epam.com>
2015-09-25 11:41:00 +0300

    Categories in list now disabled when have no children.

--- ELib/ELib.Web/Views/Home/book/BooksController.js
+++ ELib/ELib.Web/Views/Home/book/BooksController.js
@@ -6,7 +6,7 @@
 
     function BooksController(dataServiceFactory, $routeParams, $location) {
         var vm = this;
-        vm.pageCount = ($routeParams.pageCount) ? $routeParams.pageCount : "5";
+        vm.pageCount = ($routeParams.pageCount) ? $routeParams.pageCount : 5;
         vm.currPage = ($routeParams.pageNumb) ? $routeParams.pageNumb : 1;
 
         var obj = dataServiceFactory.getService('category').query();
@@ -22,14 +22,14 @@
 
 
         var parameters = {
-            pageCount: vm.pageCount,
-            pageNumb: vm.currPage,
-            query: $routeParams.query,
-            title: $routeParams.title,
+            pageCount : vm.pageCount,
+            pageNumb  : vm.currPage,
+            query     : $routeParams.query,
+            title     : $routeParams.title,
             authorName: $routeParams.author,
-            genre: $routeParams.genre,
-            subgenre: $routeParams.subgenre,
-            year: $routeParams.year
+            genre     : $routeParams.genre,
+            subgenre  : $routeParams.subgenre,
+            year      : $routeParams.year
     }
 
         vm.pageChanged = pageChanged;
@@ -46,11 +46,13 @@
         })
       
         function pageChanged() {
-            $location.search("pageCount", vm.pageCount);
-            $location.search("pageNumb", vm.currPage);
+            $location.search({ "pageCount": vm.pageCount, "pageNumb": vm.currPage });
         }
 
 
+        vm.NodeButtonState = function (node) {
+
+        }
       
 
         function preProcessCategories(children, level) {
