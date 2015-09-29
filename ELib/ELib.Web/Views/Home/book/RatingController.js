03225f9ffb02ac6db447daa06a30380794727f7e
Дмитрий <dim_skrip@mail.ru>
2015-09-29 11:32:16 +0300

    Rating been fixed without a vote for  unauthorized user

--- ELib/ELib.Web/Views/Home/book/RatingController.js
+++ ELib/ELib.Web/Views/Home/book/RatingController.js
@@ -1,17 +1,25 @@
 ﻿(function () {
     angular.module("elib").controller('RatingController', RatingController)
 
-    RatingController.$inject = ["dataServiceFactory", "CurrentProfileFactory"];
+    RatingController.$inject = ["dataServiceFactory", "CurrentProfileFactory", "authServiceFactory"];
 
-    function RatingController(dataServiceFactory, CurrentProfileFactory) {
+    function RatingController(dataServiceFactory, CurrentProfileFactory, authServiceFactory) {
+
 
         var vm = this;
-             
+
+        authServiceFactory.fillAuthData();
+
+        if (authServiceFactory.authentication.isAuth) {
+            vm.editableRating = true;
+        }
+        else { vm.editableRating = false; }
+        
         vm.basedOn = 100;
         vm.starsCount = 5;
         vm.iconClass = 'fa fa-star';
         vm.showGrade = true;
-        vm.editableRating = true;
+        
 
     }
 
