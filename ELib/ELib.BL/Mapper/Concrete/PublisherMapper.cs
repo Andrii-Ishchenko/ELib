bf4a78b254dd6fbb430ff2642818b7c77c791aa8
Snizhana <lavliess@rambler.ru>
2015-10-02 11:06:49 +0300

    add publisher mapper

---
+++ ELib/ELib.BL/Mapper/Concrete/PublisherMapper.cs
@@ -0,0 +1,18 @@
+﻿using ELib.BL.DtoEntities;
+using ELib.Domain.Entities;
+
+namespace ELib.BL.Mapper.Concrete
+{
+    public class PublisherMapper : IMapper<Publisher, PublisherDto>
+    {
+        public Publisher Map(PublisherDto input)
+        {
+            return new Publisher() { Id = input.Id, Name = input.Name };
+        }
+
+        public PublisherDto Map(Publisher input)
+        {
+            return new PublisherDto() { Id = input.Id, Name = input.Name };
+        }
+    }
+}
