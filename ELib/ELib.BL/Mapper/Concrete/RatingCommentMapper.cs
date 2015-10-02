c995fd6f0190feeda020da39f4521e85dd914fb8
Yaroslav Udovichenko <Yaroslav_Udovichenko@epam.com>
2015-10-02 12:07:02 +0300

    add PersonMapper RatingMapper RatingCommentMapper

---
+++ ELib/ELib.BL/Mapper/Concrete/RatingCommentMapper.cs
@@ -0,0 +1,18 @@
+﻿using ELib.BL.DtoEntities;
+using ELib.Domain.Entities;
+
+namespace ELib.BL.Mapper.Concrete
+{
+    public class RatingCommentMapper : IMapper<RatingComment, RatingCommentDto>
+    {
+        public RatingCommentDto Map(RatingComment input)
+        {
+            return new RatingCommentDto() {Id = input.Id, CommentId = input.CommentId, UserId = input.UserId, IsLike = input.IsLike };
+        }
+
+        public RatingComment Map(RatingCommentDto input)
+        {
+            return new RatingComment() { Id = input.Id, CommentId = input.CommentId, UserId = input.UserId, IsLike = input.IsLike};
+        }
+    }
+}
