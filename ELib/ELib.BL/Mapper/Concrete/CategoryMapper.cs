e7047d5a2e989851a12db97d026b8f2252fb5ebf
Snizhana <lavliess@rambler.ru>
2015-10-02 12:01:37 +0300

    add mappers

---
+++ ELib/ELib.BL/Mapper/Concrete/CategoryMapper.cs
@@ -0,0 +1,33 @@
+﻿using ELib.BL.DtoEntities;
+using ELib.Domain.Entities;
+using System;
+
+namespace ELib.BL.Mapper.Concrete
+{
+    public class CategoryMapper : IMapper<Category, CategoryDto>
+    {
+        public Category Map(CategoryDto input)
+        {
+            return new Category()
+            {
+                Id = input.Id,
+                Name = input.Name,
+                ParentId = input.ParentId,
+                Level = input.Level,
+                BookCount = input.BookCount
+            };
+        }
+
+        public CategoryDto Map(Category input)
+        {
+            return new CategoryDto()
+            {
+                Id = input.Id,
+                Name = input.Name,
+                ParentId = input.ParentId,
+                Level = input.Level,
+                BookCount = input.BookCount
+            };
+        }
+    }
+}
---
+++ ELib/ELib.BL/Mapper/Concrete/SubgenreMapper.cs
@@ -0,0 +1,27 @@
+﻿using ELib.BL.DtoEntities;
+using ELib.Domain.Entities;
+using System;
+
+namespace ELib.BL.Mapper.Concrete
+{
+    public class SubgenreMapper : IMapper<Subgenre, SubgenreDto>
+    {
+        public Subgenre Map(SubgenreDto input)
+        {
+            return new Subgenre()
+            {
+                Id = input.Id,
+                Name = input.Name
+            };
+        }
+
+        public SubgenreDto Map(Subgenre input)
+        {
+            return new SubgenreDto()
+            {
+                Id = input.Id,
+                Name = input.Name
+            };
+        }
+    }
+}
