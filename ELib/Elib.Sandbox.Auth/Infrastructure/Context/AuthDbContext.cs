c2dcd662ff2d4a824a4d6b2e14cdb40a14707bd4
Ihor Herasymiv <igorgerasymiv@gmail.com>
2015-10-15 13:01:19 +0300

    add DbCOntext and Application user classes

---
+++ ELib/Elib.Sandbox.Auth/Infrastructure/Context/AuthDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Elib.Sandbox.Auth.Infrastructure.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Elib.Sandbox.Auth.Infrastructure.Context
+{
+    public class AuthDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AuthDbContext() 
+            : base("AuthDb")
+        {
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        public static AuthDbContext Create()
+        {
+            return new AuthDbContext();
+        }
+    }
+}
---
+++ ELib/Elib.Sandbox.Auth/Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Elib.Sandbox.Auth.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+    }
+}
