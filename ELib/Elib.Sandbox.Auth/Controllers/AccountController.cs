43b63ea0c64db7bdd0c31f0afdacac25ffbeb827
Ihor Herasymiv <igorgerasymiv@gmail.com>
2015-10-16 14:14:06 +0300

    add basic functionality to account controller

--- ELib/Elib.Sandbox.Auth/Controllers/AccountController.cs
+++ ELib/Elib.Sandbox.Auth/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Elib.Sandbox.Auth.Controllers
@@ -69,12 +70,60 @@ namespace Elib.Sandbox.Auth.Controllers
 
             return null;
         }
-        /*
         [Route("user/{id:guid}", Name = "GetUserById")]
-        public IHttpActionResult GetUser(string id)
+        public async Task<IHttpActionResult> GetUser(string Id)
         {
-            
+            var user = await this.AppUserManager.FindByIdAsync(Id);
+
+            if (user != null)
+            {
+                return Ok(this.TheModelFactory.Create(user));
+            }
+
+            return NotFound();
+
         }
-        */
+
+        [Route("user/{username}")]
+        public async Task<IHttpActionResult> GetUserByName(string username)
+        {
+            var user = await this.AppUserManager.FindByNameAsync(username);
+
+            if (user != null)
+            {
+                return Ok(this.TheModelFactory.Create(user));
+            }
+
+            return NotFound();
+
+        }
+
+        [Route("create")]
+        public async Task<IHttpActionResult> CreateUser(CreateUserBindingModel createUserModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new ApplicationUser()
+            {
+                UserName = createUserModel.Username,
+                Email = createUserModel.Email,
+                FirstName = createUserModel.FirstName,
+                LastName = createUserModel.LastName,
+            };
+
+            IdentityResult addUserResult = await this.AppUserManager.CreateAsync(user, createUserModel.Password);
+
+            if (!addUserResult.Succeeded)
+            {
+                return GetErrorResult(addUserResult);
+            }
+
+            Uri locationHeader = new Uri(Url.Link("GetUserById", new { id = user.Id }));
+
+            return Created(locationHeader, TheModelFactory.Create(user));
+        }
     }
 }
