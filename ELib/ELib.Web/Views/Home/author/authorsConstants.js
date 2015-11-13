a682b8585187c038aff64693fdd865ad742acc3c
Snizhana Bilovytska <snizhana.bilovytska@gmail.com>
2015-11-13 12:33:03 +0200

    fix

--- ELib/ELib.Web/Views/Home/author/authorsConstants.js
+++ ELib/ELib.Web/Views/Home/author/authorsConstants.js
@@ -1,13 +1,15 @@
 ﻿(function () {
     angular.module("elib")
-           .constant('AUTHOR_CONST', {
-               'MAX_SIZE': '5',
-               'PAGE_COUNT': '5',
-               'FIRST_PAGE': '1',
-               'LOGIN': '/login',
-               'AUTHORS': '/authors/',
-               'AUTHOR': "/api/authors/id/author",
-               'CREATION_FAILED':"Author creation is failed",
-               'CREATION_SUCCESFUL':"Author has been created successfully, you will be redicted to author page in 2 seconds."
-           });    
+           .constant('AUTHOR_CONST',(function() {               
+               return {
+                   'MAX_SIZE': '5',
+                   'PAGE_COUNT': '5',
+                   'FIRST_PAGE': '1',
+                   'LOGIN': '/login',
+                   'AUTHORS': '/authors/',
+                   'AUTHOR': "/api/authors/id/author",
+                   'CREATION_FAILED':"Author creation is failed",
+                   'CREATION_SUCCESFUL':"Author has been created successfully, you will be redicted to author page in 2 seconds."
+               }
+           })());
 })();
