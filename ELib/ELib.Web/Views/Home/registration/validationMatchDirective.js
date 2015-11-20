219db49b4bcead63a30c5ddd43ac2b2d13a46aea
Дмитрий <dim_skrip@mail.ru>
2015-11-20 12:21:24 +0200

    Add compareTo directive

---
+++ ELib/ELib.Web/Views/Home/registration/validationMatchDirective.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    angular.module("elib")
+           .directive("compareTo", compareTo);
+
+    function compareTo() {
+        return {
+            require: "ngModel",
+            scope: {
+                otherModelValue: "=compareTo"
+            },
+            link: function (scope, element, attributes, ngModel) {
+
+                ngModel.$validators.compareTo = function (modelValue) {
+                    return modelValue == scope.otherModelValue;
+                };
+
+                scope.$watch("otherModelValue", function () {
+                    ngModel.$validate();
+                });
+            }
+        };
+    };
+})();
