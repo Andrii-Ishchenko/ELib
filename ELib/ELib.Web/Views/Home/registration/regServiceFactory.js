a12557c136b70a2890643d59283e20af8c029e21
dimskrip <dim_skrip@mail.ru>
2015-11-24 14:23:33 +0200

    Added factory regServiceFactory

---
+++ ELib/ELib.Web/Views/Home/registration/regServiceFactory.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    angular.module("elib")
+           .factory("regServiceFactory", regServiceFactory);
+
+    function regServiceFactory($http, $q) {
+        return {
+            IsUserNameAvailablle: function (userName) {
+                // Get the deferred object
+                var deferred = $q.defer();
+                // Initiates the AJAX call
+                $http({ method: 'GET', url: window.location.origin + '/api/account/IsUserNameAvailable?userName=' + userName })
+                    .success(deferred.resolve)
+                    .error(deferred.reject);
+                // Returns the promise - Contains result once request completes
+                return deferred.promise;
+            },
+            IsEmailAvailablle: function (email) {
+                // Get the deferred object
+                var deferred = $q.defer();
+                // Initiates the AJAX call
+                $http({ method: 'GET', url: window.location.origin + '/api/account/IsEmailAvailable?email=' + email })
+                    .success(deferred.resolve)
+                    .error(deferred.reject);
+                // Returns the promise - Contains result once request completes
+                return deferred.promise;
+            }
+        }
+    }
+})();
