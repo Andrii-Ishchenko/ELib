82c6a08545c73c4fffecc539325b9932f34d4c44
dimskrip <dim_skrip@mail.ru>
2015-11-24 14:22:53 +0200

    Added directive checkUserNameAvailableDirective

---
+++ ELib/ELib.Web/Views/Home/registration/checkUserNameAvailableDirective.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    angular.module("elib")
+        .directive('useravail', useravail);
+
+    function useravail(regServiceFactory) {
+        var directive = {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function (scope, element, attrs, ngModel) {
+                element.on('blur', function (evt) {
+
+                    if (!ngModel || !element.val()) return;
+                    var curValue = element.val();
+
+                    regServiceFactory.IsUserNameAvailablle(curValue)
+                    .then(function (response) {
+
+                        ngModel.$setValidity('unique', response);
+                    }, function () {
+                        ngModel.$setValidity('unique', true);
+                    });
+
+                });
+            }
+        }
+        return directive;
+    };
+})();
