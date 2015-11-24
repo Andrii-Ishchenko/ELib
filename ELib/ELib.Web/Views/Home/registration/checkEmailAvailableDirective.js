634088823c25aacf45ccbe3c4ea0870183e158ae
dimskrip <dim_skrip@mail.ru>
2015-11-24 14:22:24 +0200

    Added directive checkEmailAvailableDirective

---
+++ ELib/ELib.Web/Views/Home/registration/checkEmailAvailableDirective.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    angular.module("elib")
+        .directive('emailavail', emailavail);
+
+    function emailavail(regServiceFactory) {
+        var directive = {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function (scope, element, attrs, ngModel) {
+                element.on('blur', function (evt) {
+
+                    if (!ngModel || !element.val()) return;
+                    var curValue = element.val();
+
+                    regServiceFactory.IsEmailAvailablle(curValue)
+                    .then(function (response) {
+
+                        ngModel.$setValidity('unique', response);
+                    }, function () {
+                        ngModel.$setValidity('unique', true);
+                    });
+                });
+            }
+        }
+        return directive;
+    };
+})();
